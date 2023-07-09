be9adfd635ff103cf15aff91ed0681347ad21c18
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-07-09 19:09:23 +0300

    CRPT: added ecdh signature implementation

--- README.md
+++ README.md
@@ -1,43 +1,34 @@
 # Working with elliptic curves (custom)
 
-## Using secp192r1 params for keys generation and ECDH
+## Using secp192r1 params for keys generation and ECDH Signature
 
 ### The **EllipticCurve** class is designed to wrapp tinyec library that implements all elliptic curve logic. There no use standard approved curves, other libs provide all options for using ecc. Here is class and examples for understanding only.
+### The **EllipticCurveSignature** class is designed to create ECC Asymmetric Key Pair, sign messages and verify signatures.
+
 
 ### Implemented functions:
-- Checks:
-    - is_on_curve_check
-    - equals_points
-- Math operations:
-    - double_ec_point
-    - scalar_mult
-    - add_ec_points
-- Internal validation:
-    - _validate_positive_int
-    - _validate_mod
-    - _validate_coordinates
-- Other:
-    - compress
-    - print_ec_point
-    - get_generator_point
-
-### To run localy:
+- get_key_pair
+- sign_message
+- verify_signed_message
+- helper functions:
+    - _generate_coprime
+    - _hash_to_number
+    
+### To run locally:
 ``` 
 pip install pycryptodome
 pip install primePy
 pip install tinyec
+pip install sympy
 python3 ecc.py
 ```
 
 ### Execution result:
-![](/img/2023-07-02_20-59.png "Test")
+![](/img/2023-07-09_19-01.png "Test")
 
 ### Resources 
-https://wizardforcel.gitbooks.io/practical-cryptography-for-developers-book/content/asymmetric-key-ciphers/elliptic-curve-cryptography-ecc.html
+Description ECC Signature Algorithm:
+- https://wizardforcel.gitbooks.io/practical-cryptography-for-developers-book/content/digital-signatures/eddsa-and-ed25519.html
 
-http://www.secg.org/sec1-v2.pdf
-
-
-https://www.youtube.com/watch?v=F3zzNa42-tQ
-
-https://www.youtube.com/watch?v=NF1pwjL9-DE
+Demonstration ECC Signature Algorithm:
+- https://www.youtube.com/watch?v=6TI5YOpnrgI
--- ecc.py
+++ ecc.py
@@ -1,6 +1,7 @@
 from tinyec.ec import SubGroup, Curve, Point
 from primePy import primes
-from Crypto.Util import number
+from sympy import mod_inverse, Mod, gcd, ntheory
+import hashlib
 
 
 # Wrapper class that used for educational purpose only (full crypto libs had already implemented all ec cryptography)
@@ -74,39 +75,147 @@ class EllipticCurve:
     def print_ec_point(point):
         print(f"({point.x}, {point.y})")
 
+    # Perform the addition of ANY points that belongs to curve to another point that belongs to curve
+    def add_ec_points_extended(self, x1, y1, x2, y2):
+        self.is_on_curve_check(x=x1, y=y1)
+        self.is_on_curve_check(x=x2, y=y2)
+        init_point = Point(curve=self.curve, x=x1, y=y1)
+        addition_point = Point(curve=self.curve, x=x2, y=y2)
+        new_point = init_point.__add__(other=addition_point)
+        return [new_point.x, new_point.y]
 
-# Using secp192r1 params
-p = 0xfffffffffffffffffffffffffffffffeffffffffffffffff
-a = 0xfffffffffffffffffffffffffffffffefffffffffffffffc
-b = 0x64210519e59c80e70fa7e9ab72243049feb8deecc146b9b1
-x = 0x188da80eb03090f67cbf20eb43a18800f4ff0afd82ff1012
-y = 0x07192b95ffc8da78631011ed6b24cdd573f977a11e794811
-n = 0xffffffffffffffffffffffff99def836146bc9b1b4d22831
-h = 0x1
+    # Perform the multiplication of ANY point to some number (addition point k times)
+    def scalar_mult_extended(self, k, x, y):
+        init_point = Point(curve=self.curve, x=x, y=y)
+        point = k * init_point
+        return point
 
-el_test_curve = EllipticCurve(x=x, y=y, p=p, a=a, b=b, n=n, h=h)
-print("Curve: ", el_test_curve.curve)
 
-print("Base point G:", el_test_curve.get_generator_point())
+class EllipticCurveSignature:
+    # # ##############################################
+    # # Generate Keys & Fill Curve Params
+    #
+    def __init__(self):
+        # Using secp192r1 params
+        p = 0xfffffffffffffffffffffffffffffffeffffffffffffffff
+        a = 0xfffffffffffffffffffffffffffffffefffffffffffffffc
+        b = 0x64210519e59c80e70fa7e9ab72243049feb8deecc146b9b1
+        x = 0x188da80eb03090f67cbf20eb43a18800f4ff0afd82ff1012
+        y = 0x07192b95ffc8da78631011ed6b24cdd573f977a11e794811
+        n = 0xffffffffffffffffffffffff99def836146bc9b1b4d22831
+        h = 0x1
 
-alicePrivKey = int(input("Input private key [0-(n-1)]:"))
-print("Alice private key:", alicePrivKey)
-alicePubKey = el_test_curve.scalar_mult(k=alicePrivKey)
-alice_ec = EllipticCurve(x=alicePubKey.x, y=alicePubKey.y, p=p, a=a, b=b, n=n, h=h)
-print("Alice public key:", alicePubKey.x, alicePubKey.y)
+        # init curve
+        el_test_curve = EllipticCurve(x=x, y=y, p=p, a=a, b=b, n=n, h=h)
 
-bobPrivKey = int(input("Input private key [0-(n-1)]:"))
-print("Bob private key:", alicePrivKey)
-bobPubKey = el_test_curve.scalar_mult(k=bobPrivKey)
-bob_ec = EllipticCurve(x=bobPubKey.x, y=bobPubKey.y, p=p, a=a, b=b, n=n, h=h)
-print("Bob public key:", bobPubKey.x, bobPubKey.y)
+        # private key = random [0...n-1]
+        privKey = self._generate_coprime(n=n)
 
-print("Now exchange the public keys (e.g. through Internet)")
+        # public key = G * privKey
+        pubKey = el_test_curve.scalar_mult(k=privKey)
 
-aliceSharedKey = bob_ec.scalar_mult(alicePrivKey)
-print("Alice shared key:", aliceSharedKey.x, aliceSharedKey.y)
+        self.privKey = privKey
+        self.pubKey = pubKey
+        self.n = n
+        self.curve = el_test_curve
 
-bobSharedKey = alice_ec.scalar_mult(bobPrivKey)
-print("Bob shared key:", bobSharedKey.x, bobSharedKey.y)
+    def get_key_pair(self):
+        return [self.privKey, self.pubKey]
 
-print("Equal shared keys:", aliceSharedKey.x == bobSharedKey.x)
+    # # ##############################################
+    # # Helpers
+    #
+    def _generate_coprime(self, n):
+        while True:
+            # Generate a random prime number
+            p = ntheory.randprime(1, n)
+
+            # Check if p is coprime with n
+            if gcd(p, n) == 1:
+                return p
+
+    def _hash_to_number(self, hash_string):
+        hash_bytes = hashlib.sha256(hash_string.encode()).digest()
+        hash_number = int.from_bytes(hash_bytes, byteorder='big')
+        return hash_number
+
+    # # ##############################################
+    # # ECDSA Sign
+    #
+    def sign_message(self, message, privKey):
+        data_bytes = message.encode('utf-8')
+        sha256_hash = self._hash_to_number(hashlib.sha256(data_bytes).hexdigest())
+
+        # Generate random k = random [1..n-1]
+        k = self._generate_coprime(n=self.n)
+
+        # Calculate random point R = k * G
+        R = self.curve.scalar_mult(k=k)
+        r = R.x
+
+        # calculate k^-1 mod n
+        k_inverse_n = mod_inverse(a=k, m=self.n)
+
+        # calculate (h + r * privateKey) mod n
+        addition = Mod(sha256_hash + r * privKey, self.n)
+
+        # calculate s = (k^-1 * (h + r * privateKey)) mod n
+        s = Mod(k_inverse_n * addition, self.n)
+        return [r, s]
+
+    # # ##############################################
+    # # ECDSA Verify Signature
+    #
+    def verify_signed_message(self, message, pubKey, s, r):
+        data_bytes = message.encode('utf-8')
+        sha256_hash = self._hash_to_number(hashlib.sha256(data_bytes).hexdigest())
+
+        # calculate s^-1 mod n
+        s1 = mod_inverse(a=s, m=self.n)
+
+        # Recover the random point used during the signing R_recovered = (hash * s1)mod n * G + (r * s1)mod n * pubKey
+        hash_s1 = int(Mod(sha256_hash * s1, self.n))
+        r_s1 = int(Mod(r * s1, self.n))
+
+        hash_s1_G = self.curve.scalar_mult(k=hash_s1)
+        r_s1_pubKey = self.curve.scalar_mult_extended(k=r_s1, x=pubKey.x, y=pubKey.y)
+
+        R_recovered = self.curve.add_ec_points_extended(x1=hash_s1_G.x, y1=hash_s1_G.y, x2=r_s1_pubKey.x,
+                                                        y2=r_s1_pubKey.y)
+
+        return R_recovered[0] == r
+
+
+print("####################################################################################")
+print("Generate Keys")
+print()
+
+ellipticCurveSignature = EllipticCurveSignature()
+key_pair = ellipticCurveSignature.get_key_pair()
+
+private = key_pair[0]
+public = key_pair[1]
+
+print("PrivKey:", private)
+print("PubKey:", public)
+
+print()
+print("####################################################################################")
+print("ECDSA Sign")
+
+msg = input("Input message that need to be signed:")
+
+signature = ellipticCurveSignature.sign_message(message=msg, privKey=private)
+
+r = signature[0]
+s = signature[1]
+
+print("Signature {r, s}:", r, s)
+
+print()
+print("####################################################################################")
+print("ECDSA Verify Signature")
+
+verification = ellipticCurveSignature.verify_signed_message(message=msg, pubKey=public, s=s, r=r)
+
+print("Signature is valid?", verification)
